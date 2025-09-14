ddd6ca61317ff1c8a9a53525a988488aeb7d617f
箕迫　拓真 <takumamiisako@MiisakoTakumaMacBook-Air.local>
2025-09-14 19:01:10 +0900

    Fix merge conflicts and add seedersFix merge conflicts and clean project

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Company;
+
+class CompanySeeder extends Seeder
+{
+    public function run(): void
+    {
+        Company::insert([
+            ['company_name' => 'コカ・コーラ'],
+            ['company_name' => 'サントリー'],
+            ['company_name' => 'キリン'],
+            ['company_name' => 'アサヒ'],
+            ['company_name' => 'ダイドー'],
+            ['company_name' => 'ポッカサッポロ'],
+            ['company_name' => '伊藤園'],
+            ['company_name' => '雪印メグミルク'],
+            ['company_name' => '森永'],
+            ['company_name' => 'UCC'],
+            ['company_name' => 'ネスレ'],
+        ]);
+    }
+}
--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -9,22 +9,22 @@ class ProductsTableSeeder extends Seeder
 {
     public function run()
     {
-        Product::create([
-            'company_id' => 1, // 実際の companies テーブルのIDに合わせる
-            'product_name' => 'サンプル商品1',
-            'price' => 1000,
-            'stock' => 10, // 在庫数の例
-            'comment' => 'テスト用の商品です',
-            'img_path' => null,
-        ]);
-
-        Product::create([
-            'company_id' => 2, // 実際の companies テーブルのIDに合わせる
-            'product_name' => 'サンプル商品2',
-            'price' => 2000,
-            'stock' => 5,
-            'comment' => 'ダミーデータです',
-            'img_path' => null,
-        ]);
+        $products = [
+            ['company_id' => 1, 'product_name' => 'コーラ', 'price' => 130, 'stock' => 10],
+            ['company_id' => 2, 'product_name' => 'サイダー', 'price' => 120, 'stock' => 5],
+            ['company_id' => 3, 'product_name' => 'お茶', 'price' => 110, 'stock' => 4],
+            ['company_id' => 4, 'product_name' => 'ミネラルウォーター', 'price' => 100, 'stock' => 9],
+            ['company_id' => 5, 'product_name' => '炭酸水', 'price' => 110, 'stock' => 7],
+            ['company_id' => 6, 'product_name' => 'スポーツドリンク', 'price' => 140, 'stock' => 15],
+            ['company_id' => 7, 'product_name' => 'おしるこ', 'price' => 120, 'stock' => 8],
+            ['company_id' => 8, 'product_name' => 'ポタージュ', 'price' => 120, 'stock' => 2],
+            ['company_id' => 9, 'product_name' => 'いちごオレ', 'price' => 140, 'stock' => 12],
+            ['company_id' => 10, 'product_name' => 'コーヒー', 'price' => 120, 'stock' => 34],
+            ['company_id' => 11, 'product_name' => 'メロンソーダ', 'price' => 160, 'stock' => 15],
+        ];
+    
+        foreach ($products as $product) {
+            Product::create($product);
+        }
     }
-}
+}    
